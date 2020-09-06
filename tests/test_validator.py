59c3172395c29fb29db71727cd6b266b715a64cb
lowkorn <hanif.hg@gmail.com>
2020-09-06 13:43:37 +0700

    💯add validate name

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,25 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("ปอซ่า"))
+
+    def test_validate_name_with_invalid_input_string_of_int(self):
+        self.assertEqual(False, validate_name("007"))
+
+    def test_validate_name_with_invalid_input_contained_string_of_int(self):
+        self.assertEqual(False, validate_name("ชายปอzaa007"))
+
+    def test_validate_name_with_invalid_input_spacial_char(self):
+        self.assertEqual(False, validate_name("มีไรหรอ!"))
+
+    def test_validate_name_with_invalid_input_spacial_char_more_one(self):
+        self.assertEqual(False, validate_name("$อะไร@เนี้ย#"))
+
+    def test_validate_name_with_empty_input(self):
+        self.assertEqual(False, validate_name(""))
+
+    def test_validate_name_with_without_space(self):
+        self.assertEqual(False, validate_name("ปอ ซ่า"))
 
 
 if __name__ == '__main__':
